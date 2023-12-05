8e49400ac645e60a628f04c57843f1303052b816
Rick <rick@Ricks-MacBook-Pro-5.local>
2023-12-05 09:29:49 -0800

    Edited d05p1 to generalize rather than have specific map names

--- day05/day05.py
+++ day05/day05.py
@@ -1,6 +1,42 @@
 import re
 import numpy as np
 
+sample = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+
+
 def build_map(starting_line, lines):
     still_building = True
     line_idx = starting_line
@@ -17,6 +53,7 @@ def build_map(starting_line, lines):
     arr = arr[arr[:,1].argsort()]
     return arr, line_idx
 
+
 def map_value(v_in, map):
     if v_in > map[-1,1]:
         row = len(map[:,1])
@@ -29,29 +66,25 @@ def map_value(v_in, map):
         v_out = v_in
     return v_out
 
-def loc_from_seed(seed):
-    soil = map_value(seed, seed_to_soil_map)
-    fertilizer = map_value(soil, soil_to_fertilizer_map)
-    water = map_value(fertilizer, fertilizer_to_water_map)
-    light = map_value(water, water_to_light_map)
-    temp = map_value(light, light_to_temp_map)
-    humidity = map_value(temp, temp_to_humidity_map)
-    loc = map_value(humidity, humidity_to_loc_map)
-    return loc
+
+def loc_from_seed(seed, maps):
+    source = seed
+    for map in maps:
+        dest = map_value(source, map)
+        source = dest
+    return dest
 
 with open("input05.txt") as f:
     lines = f.read().splitlines()
     seeds = [int(x) for x in re.findall(r"\d+", lines[0])]
     current_line = 3
-    seed_to_soil_map, current_line = build_map(current_line, lines)
-    soil_to_fertilizer_map, current_line = build_map(current_line + 2, lines)
-    fertilizer_to_water_map, current_line = build_map(current_line + 2, lines)
-    water_to_light_map, current_line = build_map(current_line + 2, lines)
-    light_to_temp_map, current_line = build_map(current_line + 2, lines)
-    temp_to_humidity_map, current_line = build_map(current_line + 2, lines)
-    humidity_to_loc_map, current_line = build_map(current_line + 2, lines)
+    maps = []
+    # Build all the maps
+    for map_idx in range(7):
+        maps.append([None])
+        maps[map_idx], current_line = build_map(current_line + 2, lines)
 
-locs = [loc_from_seed(seed) for seed in seeds]
-print(min(locs))
+    locs = [loc_from_seed(seed, maps) for seed in seeds]
+    print(min(locs))
 
 
