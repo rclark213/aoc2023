49b4afb56e14a849f261d86e19ec93425774b1ae
Rick C <rick@Ricks-MacBook-Pro-5.local>
2023-12-09 15:28:07 -0800

    Refined Day 9

--- day09/day09.py
+++ day09/day09.py
@@ -2,32 +2,14 @@ import re
 
 
 def diff_down(seq_in):
-    seq_list_down = [seq_in]
-    at_zero = False
-    print(seq_in)
-    while not at_zero:
-        seq_out = [None] * (len(seq_in) - 1)
-        for i in range(len(seq_out)):
-            seq_out[i] = seq_in[i+1] - seq_in[i]
-        seq_list_down.append(seq_out)
-        seq_in = seq_out
-        if set(seq_out) == {0}:
-            at_zero = True
-    seq_list_up = seq_list_down[::-1]
-    seq_list_up_rev = [seq[::-1] for seq in seq_list_up]
-    for i in range(len(seq_list_up) - 1):
-        current_caboose = seq_list_up[i][-1]
-        next_caboose = seq_list_up[i+1][-1]
-        new_caboose = next_caboose + current_caboose
-        seq_list_up[i+1].append(new_caboose)
-    part_1 = new_caboose
-    for i in range(len(seq_list_up_rev) - 1):
-        current_caboose = seq_list_up_rev[i][-1]
-        next_caboose = seq_list_up_rev[i+1][-1]
-        new_caboose = next_caboose - current_caboose
-        seq_list_up_rev[i+1].append(new_caboose)
-    part_2 = new_caboose
-    return part_1, part_2
+    seqs = [seq_in]
+    while set(seqs[-1]) != {0}:
+        seqs.append([y-x for x, y in zip(seqs[-1], seqs[-1][1:])])
+    right_total = sum([seq[-1] for seq in seqs])
+    left_total = 0
+    for seq in seqs[-1::-1]:
+        left_total = seq[0] - left_total
+    return right_total, left_total
 
 
 with open("input09.txt") as f:
@@ -36,8 +18,7 @@ with open("input09.txt") as f:
 part_1_sum = 0
 part_2_sum = 0
 for line in lines:
-    seq_string = re.split(r" ", line)
-    seq_int = [int(x) for x in seq_string]
+    seq_int = [int(x) for x in re.split(r" ", line)]
     part_1, part_2 = diff_down(seq_int)
     part_1_sum += part_1
     part_2_sum += part_2
